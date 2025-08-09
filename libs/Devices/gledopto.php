be8412e8b21c5f1664bedc9bb29e3ec33e6d79a3
Andreas Hahn <31884396+skyslasher@users.noreply.github.com>
2025-08-09 20:31:20 +0200

    Update gledopto.php
    Added GL-G-002P

--- libs/Devices/gledopto.php
+++ libs/Devices/gledopto.php
@@ -3,6 +3,40 @@
 declare(strict_types=1);
 
 return [
+    'GL-G-002P' => [
+        'Power' => [
+            'Name'                   => 'State',
+            'VariableProfile'        => '~Switch',
+            'VariableType'           => VARIABLETYPE_BOOLEAN,
+            'Action'                 => 'tasmota',
+            'ActionCommand'          => 'Power',
+            'SearchString'           => 'Power'
+        ],
+        'Dimmer' => [
+            'Name'                   => 'Brightness',
+            'VariableProfile'        => '~Intensity.255',
+            'VariableType'           => VARIABLETYPE_INTEGER,
+            'Action'                 => 'tasmota',
+            'ActionCommand'          => 'Dimmer',
+            'SearchString'           => 'Dimmer'
+        ],
+        'Color' => [
+            'Name'                   => 'Color',
+            'VariableProfile'        => 'HexColor',
+            'VariableType'           => VARIABLETYPE_INTEGER,
+            'Action'                 => 'tasmota',
+            'ActionCommand'          => 'Color',
+            'SearchString'           => 'X'
+        ],
+        'CT' => [
+            'Name'                   => 'Color Temperature',
+            'VariableProfile'        => 'T2M.ColorTemperature',
+            'VariableType'           => VARIABLETYPE_INTEGER,
+            'Action'                 => 'tasmota',
+            'ActionCommand'          => 'CT',
+            'SearchString'           => 'CT'
+        ]
+    ],
     '"GL-SD-001' => [
         'Power' => [
             'Name'                   => 'State',
